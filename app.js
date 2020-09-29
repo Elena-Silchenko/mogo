9586d0d88fe43b423b4c42c32c54f81f02acf110
Elen Silchenko <elen.silchenko@gmail.com>
2020-09-29 15:29:56 +0200

    Feature container

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-document.querySelectorAll(".numbers-").forEach(function (item) {
+document.querySelectorAll(".numbers").forEach(function (item) {
   let limit = +item.dataset.target
   let number = 0
   let increment = Math.floor(limit / 10)
   let timer = setInterval(function () {
     number += increment
     item.textContent = number
-    item.style.fontSize = Math.max((number / limit), .9) + 'em'
     
     if (number > limit) {
       increment = -1
