8e9d74ac176c36d95598bbe80fde77d2b8299617
Elen Silchenko <elen.silchenko@gmail.com>
2020-09-29 18:47:37 +0200

    Numbers animation

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-document.querySelectorAll(".numbers").forEach(function (item) {
-  let limit = +item.dataset.target
-  let number = 0
-  let increment = Math.floor(limit / 10)
-  let timer = setInterval(function () {
-    number += increment
-    item.textContent = number
-    
-    if (number > limit) {
-      increment = -1
-    }
-
-    if (number == limit) {
+function countNumbers() {
+  document.querySelectorAll(".numbers").forEach(function (item) {
+    let limit = +item.dataset.target
+    let number = 0
+    let increment = Math.floor(limit / 10)
+    let timer = setInterval(function () {
+      number += increment
       item.textContent = number
-      clearInterval(timer)
-    }
-  }, 100)
+    
+      if (number > limit) {
+        increment = -1
+      }
+
+      if (number == limit) {
+        item.textContent = number
+        clearInterval(timer)
+      }
+    }, 100)
+  })
+}
+
+const facts = document.querySelector("#facts")
+let numbersStarted = false
+window.addEventListener("scroll", function () {
+  if (window.scrollY + window.innerHeight > facts.offsetTop + facts.offsetHeight && !numbersStarted) { 
+    countNumbers() 
+    numbersStarted = true
+  }
 })
 
 function accordionUpdateStatusClasses(e) {
