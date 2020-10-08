632088d335cbffde2a8ad22fa45cae7263e1db02
Elen Silchenko <elen.silchenko@gmail.com>
2020-10-08 20:20:19 +0200

    Progress bar

--- app.js
+++ app.js
@@ -45,4 +45,29 @@
 })();
 
 (function () {
+  const progressBars = document.querySelectorAll(".progress-bar")
+  const blocks = [
+    document.querySelector("#about-us"),
+    document.querySelector("#facts"),
+    document.querySelector("#who-we-are"),
+    document.querySelector("#what-people-say")
+  ]
+  window.addEventListener("scroll", () => {
+    requestAnimationFrame(() => {
+      blocks.forEach((block, index) => {
+        let progress = progressBars[index]
+        let blockNext = blocks[index + 1]
+        let height = document.body.clientHeight - block.offsetTop - window.innerHeight
+        if (blockNext) height = blockNext.offsetTop - block.offsetTop
+
+        if (window.scrollY > block.offsetTop + height) {
+          progress.style.setProperty("--progress", 100 + "%")
+        } else if (window.scrollY > block.offsetTop) {
+          progress.style.setProperty("--progress", (100 * (window.scrollY - block.offsetTop) / height) + "%")
+        } else {
+          progress.style.setProperty("--progress", 0 + "%")
+        }
+      })
+    })
+  })
 })();
